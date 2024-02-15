1ce02e64b5f176674e7feeb5f89fe8a2430f606f
Ezequiel Massa <ezequielmassa.dev@gmail.com>
2024-02-15 19:31:59 -0300

    refactorizado codigo base

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
-
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
+import Home from './page/home/Home'
 
 function App() {
-  return (
-    <>
- 
-    </>
-  );
+	return (
+		<>
+			<Home></Home>
+		</>
+	)
 }
 
-export default App;
+export default App
