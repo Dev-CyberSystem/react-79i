611b5797458785b659d4ad64584a65d14afd93c5
Diego Olmi <diegolmi@hotmail.com>
2024-02-14 21:22:39 -0300

    Componentes

---
+++ src/page/home/Home.jsx
@@ -0,0 +1,15 @@
+
+const Home = () => {
+  return (
+    {/* 
+      Landing page
+      -Navbar
+      -carrousel
+      -cards
+      -section (Informacion)
+      -footer */}
+
+  )
+}
+
+export default Home
