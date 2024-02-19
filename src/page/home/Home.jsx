43090df94ef4e0d9fb2f061976b79c1e775ed13e
FedericoFrau <fedeafrau@gmail.com>
2024-02-18 21:01:09 -0300

    add components files and Navbar component

--- src/page/home/Home.jsx
+++ src/page/home/Home.jsx
@@ -1,18 +1,20 @@
+import { Navbarbar } from "../../components/navbar/Navbarbar";
 
 const Home = () => {
   return (
     <>
+    <Navbarbar/>
+    
     {/* 
       Landing page
       -Navbar
       -carrousel
       -cards
       -section (Informacion)
-    -footer */}
+      -footer */}
       <h1>Home</h1>
     </>
-
-  )
+  );
 }
 
 export default Home
