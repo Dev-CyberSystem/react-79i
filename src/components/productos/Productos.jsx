d9c47209edc99b3a3a1b3d39c8295917f1ec8a8a
ignacio skib <nanoskibski@gmail.com>
2024-02-21 19:59:52 -0300

    Primer commit

--- src/components/productos/Productos.jsx
+++ src/components/productos/Productos.jsx
@@ -1,24 +1,2 @@
-import PropTypes from "prop-types";
 
-const Productos = ({ funcionProps, club }) => {
-  const { nombre, estadio } = club;
-
-  return (
-    <>
-      <h1>Componente de Productos</h1>
-      <h2>
-        Nombre: {nombre} - Estadio: {estadio}
-      </h2>
-      <button onClick={funcionProps}>Ejecuta la funcion</button>
-    </>
-  );
-};
-
-Productos.propTypes = {
-  name: PropTypes.string.isRequired,
-  edad: PropTypes.number,
-  funcionProps: PropTypes.func,
-  club: PropTypes.object,
-};
-
-export default Productos;
+<h2>hola mundo</h2>
