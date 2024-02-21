c23f13515823b304ba999ff5a5471bf39023482d
Ezequiel Massa <ezequielmassa.dev@gmail.com>
2024-02-20 21:12:41 -0300

    rafactor Counter

--- src/page/counter/Counter.jsx
+++ src/page/counter/Counter.jsx
@@ -5,25 +5,11 @@ import CounterDisplay from './components/CounterDisplay'
 const Counter = () => {
 	const [counter, setCounter] = useState(0)
 
-	const increase = () => {
-		setCounter((prev) => prev + 1)
-	}
-	const decrease = () => {
-		setCounter((prev) => {
-			if (prev > 0) {
-				return prev - 1
-			}
-			return prev
-		})
-	}
-
 	return (
 		<main className='min-vh-100 d-flex flex-column  align-items-center justify-content-center gap-4'>
 			<h1>Counter</h1>
 			<CounterDisplay counter={counter}></CounterDisplay>
-			<CounterControls
-				increase={increase}
-				decrease={decrease}></CounterControls>
+			<CounterControls setCounter={setCounter}></CounterControls>
 		</main>
 	)
 }
--- src/page/counter/components/CounterControls.jsx
+++ src/page/counter/components/CounterControls.jsx
@@ -1,9 +1,22 @@
 import { Button } from 'react-bootstrap'
 
-const CounterControls = ({ increase, decrease }) => {
+const CounterControls = ({ setCounter }) => {
+	const increment = () => {
+		setCounter((prev) => prev + 1)
+	}
+
+	const decrease = () => {
+		setCounter((prev) => {
+			if (prev > 0) {
+				return prev - 1
+			}
+			return prev
+		})
+	}
+
 	return (
 		<div className='d-flex gap-5 '>
-			<Button variant='success' onClick={increase}>
+			<Button variant='success' onClick={increment}>
 				+
 			</Button>
 			<Button variant='danger' onClick={decrease}>
