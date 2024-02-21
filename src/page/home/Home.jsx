b63817927ef069722659ac8345031d6fbb5cf905
Braian Mena <braianmena@gmail.com>
2024-02-21 19:43:33 -0300

    Modificacion de componente 'Home' para poder entregar tarea.

--- src/page/home/Home.jsx
+++ src/page/home/Home.jsx
@@ -1,45 +1,44 @@
-import { useState } from "react";
-import Productos from "../../components/productos/Productos";
+// import { useState } from 'react' // 'useState' no es un componente, estoy usando la funcionalidad
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navegador from '../../components/navegador/Navegador'
+import Footer from '../../components/footer/Footer';
+import CarouselHome from '../../components/carousel/Carousel';
+import Cards from '../../components/cards/Cards';
+import Information from '../../components/information/Information';
 
 const Home = () => {
-
-  //Hooks de estado. 
-  // Que es un estado? Es un objeto que contiene propiedades que son relevantes para el componente. y pueden cambiar a lo largo de la vida del componente.
-  // Que es un hook? Es una funcion que permite agregar caracteristicas a un componente funcional.
-
-  // useState  Inicializa el estado del componente y obtiene el valor actual
-  // const [state, setState] = useState(initialState);
-  const [count, setCount] = useState();
-
-  console.log(count, "estado inicial del contador")
-
-  //useEffect
-  //useContext
-  //useReducer
-  //useRef
-  //Podemos hacer custom hooks. 
-
-  const funcionProps = () => {
-    console.log("Me ejecutaron desde el componente Productos");
-  };
-
-  const club = {
-    nombre: "River",
-    estadio: "Monumental",
-  
-  };
-
-  return (
+    // const [count, setCount] = useState(0)
+    return (
     <>
-      <h1>Home</h1>
+        <header>
+            <Navegador/>
+        </header>
 
-      <button onClick={() => setCount(count + 1)}>Aumentar</button>
-      <button onClick={() => setCount(count - 1)}>Disminuir</button>
+        <main>
+            <CarouselHome/>
+
+            
+            <h2 className='text-center mt-5'>Más Productos</h2>
+            <Cards/>
+
+            <section className='m-5 text-center '>
+                <Information/>
+            </section>
+
+            <ul>
+                <li>Navbar (v)</li>
+                <li>Carousel (v)</li>
+                <li>Cards (v)</li>
+                <li>Section (informacion)</li>
+                <li>Footer</li>
+            </ul>
+
+        </main>
 
 
-      <Productos name="Diego" edad="37" apellido funcionProps={funcionProps} club={club}/>
+        <Footer/>
     </>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
