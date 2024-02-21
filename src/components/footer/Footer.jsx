b39f262eb28643b873ef187007c0889a0703839a
Andi Orlandi <andijoorlandi>
2024-02-21 00:29:05 -0300

    resolviendo error codigo classs

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,7 +4,7 @@ export function Footer() {
   return (
     <div>
       <div
-        class="copyright text-center p-3 text-white mt-5 "
+        className="copyright text-center p-3 text-white mt-5 "
       >
         © Todos los Derechos Reservados Sinergia Selección de Personal
         
