13400869f299cc937ac3267ebf68b90f947873c2
ignacio skib <nanoskibski@gmail.com>
2024-02-23 17:59:06 -0300

    actualizando el componente de producto

--- src/components/productos/Productos.jsx
+++ src/components/productos/Productos.jsx
@@ -1,24 +1,48 @@
-import PropTypes from "prop-types";
+import React from 'react'
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import './styleProductos.css'
+import { Container } from 'react-bootstrap';
+import ZapatillaProducto from '../../assets/productos/zapatilla-productos.jpg'
 
-const Productos = ({ funcionProps, club }) => {
-  const { nombre, estadio } = club;
-
+export const Productos = () => {
   return (
     <>
-      <h1>Componente de Productos</h1>
-      <h2>
-        Nombre: {nombre} - Estadio: {estadio}
-      </h2>
-      <button onClick={funcionProps}>Ejecuta la funcion</button>
-    </>
-  );
-};
-
-Productos.propTypes = {
-  name: PropTypes.string.isRequired,
-  edad: PropTypes.number,
-  funcionProps: PropTypes.func,
-  club: PropTypes.object,
-};
-
-export default Productos;
+    <Container className='productos'>
+    <Card style={{ width: '18rem' }}>
+    <Card.Img variant="top" src={ZapatillaProducto} />
+    <Card.Body>
+      <Card.Title>Card Title</Card.Title>
+      <Card.Text>
+        Some quick example text to build on the card title and make up the
+        bulk of the card's content.
+      </Card.Text>
+      <Button variant="primary">Ver Mas</Button>
+    </Card.Body>
+  </Card>
+  <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={ZapatillaProducto} />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant="primary">Ver mas</Button>
+      </Card.Body>
+    </Card>
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={ZapatillaProducto} />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant="primary">Ver Mas</Button>
+      </Card.Body>
+    </Card>
+  </Container>
+  </>
+  )
+}
