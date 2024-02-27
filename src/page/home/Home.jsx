88374d027153e63e009ba058835dcd1399b606e8
Ezequiel Massa <ezequielmassa.dev@gmail.com>
2024-02-27 06:10:36 -0300

    refactor home

--- src/page/home/Home.jsx
+++ src/page/home/Home.jsx
@@ -14,32 +14,25 @@ const Home = () => {
 		return <Error errorMsg={error.response.data.status_message}></Error>
 	}
 
-	if (!data.results || isLoading) {
-		return <Spinner></Spinner>
-	}
-
-	const carouselItems = data.results?.slice(0, 3).map((item) => {
-		return {
-			title: item.original_title,
-			id: item.id,
-			img: item.backdrop_path,
-			description: item.overview,
-		}
-	})
-
-	const infoHome = {
-		img: carouselItems[1]?.img,
-		text: carouselItems[1]?.description,
-	}
-
 	return (
 		<>
 			<article className='container-fluid  my-2'>
 				<section className='row'>
 					<div className='col'>
-						<CarouselHome
-							itemsArray={carouselItems}
-							interval={2000}></CarouselHome>
+						{!data || !data.results || isLoading ? (
+							<Spinner />
+						) : (
+							<CarouselHome
+								itemsArray={data.results?.slice(0, 3).map((item) => {
+									return {
+										title: item.original_title,
+										id: item.id,
+										img: item.backdrop_path,
+										description: item.overview,
+									}
+								})}
+								interval={2000}></CarouselHome>
+						)}
 					</div>
 				</section>
 			</article>
@@ -48,21 +41,35 @@ const Home = () => {
 			<article className='container-md mb-5 '>
 				<section className='row'>
 					<div className='col-12 d-flex flex-wrap justify-content-center align-items-stretch gap-4'>
-						{data.results.slice(0, 10).map((movie) => (
-							<CardGeneric
-								key={movie.id}
-								elementId={movie.id}
-								cardImg={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
-								cardTitle={movie.title}
-								cardText={movie.overview}></CardGeneric>
-						))}
+						{!data || !data.results || isLoading ? (
+							<Spinner />
+						) : (
+							data.results
+								.slice(0, 10)
+								.map((movie) => (
+									<CardGeneric
+										key={movie.id}
+										elementId={movie.id}
+										cardImg={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
+										cardTitle={movie.title}
+										cardText={movie.overview}></CardGeneric>
+								))
+						)}
 					</div>
 				</section>
 			</article>
 
 			<h2 className='text-center py-2 text-light '>Nuestra recomendacion</h2>
 			<article className='container-md mb-5 '>
-				<InfoSection info={infoHome}></InfoSection>
+				{!data || !data.results || isLoading ? (
+					<Spinner />
+				) : (
+					<InfoSection
+						info={{
+							img: data.results[1].backdrop_path,
+							text: data.results[1].overview,
+						}}></InfoSection>
+				)}
 			</article>
 		</>
 	)
