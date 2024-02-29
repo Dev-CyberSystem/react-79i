6561d8defe34aa6b0b105eb42e68c626d6fe2cf7
willytucuman <tomasgroso09@hotmail.com>
2024-02-28 21:20:29 -0300

    ultimo commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,16 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Navegador from "./components/navegador/Navegador.jsx";
 import Rutas from "./routes/Rutas.jsx";
+import UsersContext from "./contextProvider/UsersContext.jsx";
 function App() {
   return (
     <>
-      <Navegador/>
-      <Rutas/>
+      <UsersContext>
+        
+          <Navegador/>
+          <Rutas/>
+        
+      </UsersContext>
     </>
   );
 }
--- src/components/usuarios/Usuarios.jsx
+++ src/components/usuarios/Usuarios.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-
+// import { useContext } from 'react';
 import './usuarios.css'
+// import UsersContext from '../../contextProvider/UsersContext';
 const Usuarios = ({name,email,id}) => {
-  
+  // const usuarios = useContext(UsersContext)
+  // console.log(usuarios)
   return (
   <>
-  
   <div className='contenedor-usuarios'>
       <Card style={{ width: '18rem',backgroundColor:"violet"}}>
       <Card.Body>
