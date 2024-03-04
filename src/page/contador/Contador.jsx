79f21632aee46f86d0f1c5fe9fb308232a92070c
georginacostilla <georginacostilla.gc@gmail.com>
2024-03-03 21:54:34 -0300

    Tarea 3 - además se mejoro el diseño de la pagina

--- src/page/contador/Contador.jsx
+++ src/page/contador/Contador.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import Button from 'react-bootstrap/Button';
 
 const Contador = ({ contador }) => {
     const [count, setCount] = useState(contador);
@@ -16,12 +17,16 @@ const Contador = ({ contador }) => {
     };
 
     return (
-        <div className='text-center'>
-            <h1>{count}</h1>
-            <button onClick={handleIncrementar}> + </button>
-            <button onClick={handleDecrementar}> - </button>
-            <button onClick={handleReseteo}> Reset </button>
-        </div>
+        <>
+            <div className='text-center mb-3'>
+                <h1 className='mt-3'>{count}</h1>
+                <Button variant="secondary" className='me-3 ms-3' onClick={handleIncrementar}><strong>Incrementar</strong></Button>
+                <Button variant="secondary" className='me-3 ms-3' onClick={handleDecrementar}><strong>Decrementar</strong></Button>
+            </div>
+            <div className='text-center'>
+                <Button variant="warning" onClick={handleReseteo}>Reset</Button>
+            </div>
+        </>
     );
 };
 
